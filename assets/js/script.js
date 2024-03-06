7d21abf18521bec147ff89f911a1dda635f4ad56
Percy_182 <percy.ignacio.garcia@gmail.com>
2024-03-06 16:32:24 -0300

    Desafio_Evaluado_M4D4

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// getElementById de JQuery
+$("#musica").attr(
+  "src",
+  "https://www.youtube-nocookie.com/embed/nyWbun_PbTc?si=UXwb3MwSu_-3eCok"
+);
+$("#peliculas").attr(
+  "src",
+  "https://www.youtube-nocookie.com/embed/YdhPAyApRQ4?si=ombWSiejmy1Xa58V"
+);
+$("#series").attr(
+  "src",
+  "https://www.youtube-nocookie.com/embed/HGMfpNxv5H4?si=EUeILy5mz3ZTo2YI"
+);
+// attr = establece que quiero modificar el atributo a indicar() en este caso "src",
+// seguido del valor del attr que es el link del video y para musica utilice NCP para que se pudiera ver.
